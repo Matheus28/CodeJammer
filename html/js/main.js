38596a1507c26358641851daa8b197a664ce0f5f
Matheus Valadares <me@m28.io>
2015-04-23 19:28:04 -0300

    Master server basically done

--- html/js/main.js
+++ html/js/main.js
@@ -6,6 +6,28 @@ socket.on("update", function(obj){
 	
 });
 
+socket.on("resultText", function(val){
+	$('#resultOutput').val(val);
+	$('#resultProgressContainer').hide();
+	$('#resultOutput').show();
+	$('#resultRow').show();
+});
+
+socket.on("resultProgress", function(obj){
+	if(obj.perc == null){
+		$('#resultProgressBar').addClass('progress-bar-striped active');
+		$('#resultProgressBar').css({width: '100%'});
+	}else{
+		$('#resultProgressBar').removeClass('progress-bar-striped active');
+		$('#resultProgressBar').css({width: obj.perc});
+	}
+	$('#resultProgressBar').text(obj.text);
+	$('#resultProgressContainer').show();
+	$('#resultOutput').hide();
+	$('#resultRow').show();
+});
+
+
 function submitJobs(){
 	socket.emit("createJobs", {
 		code: $('#codeInput').val(),
--- server.node.js
+++ server.node.js
@@ -5,19 +5,142 @@ var slaves = [];
 
 var queue = [];
 
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
+
+
 io.on('connection', function(socket){
 	socket.join('authed');
 	
 	socket.on("createJobs", function(obj){
+		var parser = null;
+		var splitterString = null;
 		
+		do {
+			splitterString = generateSplitterString();
+		}while(obj.inputText.indexOf(splitterString) != -1);
+		
+		try{
+			parser = InputParser.generateParser(obj.splitter, 'puts("' + splitterString + '");');
+		}catch(e){
+			socket.emit("resultText", "Invalid splitter:\n" + e.toString());
+			return;
+		}
+		
+		socket.emit("resultProgress", { text: "Compiling splitter..." });
+		
+		queue.unshift({
+			code: splitter,
+			input: obj.inputText,
+			onError: function(){
+				socket.emit("resultText", "Error while running splitter, is it correct?");
+			},
+			
+			onSuccess: function(output){
+				// Remove empty test cases
+				var tests = output.split(splitterString).filter(function(str){
+					return /\S/.test(str);
+				});
+				
+				if(tests.length == 0){
+					socket.emit("resultText", "No test cases resulted from splitter, is it correct?");
+					return;
+				}
+				
+				socket.emit("resultProgress", { text: "Identified " + tests.length + " jobs" });
+				
+				var testsJobs = [];
+				var supressOutput = false;
+				var pending = tests.length;
+				
+				function removeAllJobs(){
+					for (var i = 0; i < testsJobs.length; i++) {
+						queue.remove(testsJobs[i]);
+					}
+				}
+				
+				function printResult(){
+					var str = '';
+					for (var i = 0; i < tests.length; i++) {
+						str += 'Case #' + (i + 1) + ':';
+						if(tests[i][0] != '\n'){
+							str += ' ';
+						}
+						str += tests[i];
+					}
+					
+					socket.emit("resultText", str);
+				}
+				
+				for (var i = 0; i < tests.length; i++) {
+					(function(i){
+						var test = tests[i];
+						var job = {
+							code: obj.code,
+							input: test,
+							onError: function(){
+								if(supressOutput) return;
+								supressOutput = true;
+								socket.emit("resultText", "At least one of the jobs resulted in an error (timeout or crash). Aborted.");
+							},
+							
+							onSuccess: function(testResult){
+								if(supressOutput) return;
+								tests[i] = testResult;
+								if(--pending == 0){
+									printResult();
+								}else{
+									var perc = (100 * (tests.length - pending) / tests.length) + "%";
+									socket.emit("resultProgress", {
+										text: (tests.length - pending) + " out of " + tests.length + " jobs",
+										perc: perc
+									});
+								}
+							}
+						};
+						
+						testsJobs.push(job);
+						queue.push(job);
+						
+					})(i);
+				}
+				
+				
+				checkQueue();
+			}
+		});
+		
+		checkQueue();
 	});
 });
+
+function checkQueue(){
+	
+}
 
 function updateClients(){
 	io.to('authed').emit("update", {
 		clusterSize: slaves.length,
 		queueLength: queue.length
 	});
+}
+
+function generateSplitterString(){
+	var i = 32;
+	var str = '';
+	var CHARS = '.,;/-=[]{}*';
+	while(i--){
+		str += CHARS[~~(Math.random() * CHARS.length)];
+	}
+	return str;
 }
 
 setInterval(updateClients, 1000);
